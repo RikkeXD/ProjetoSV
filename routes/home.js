16675432077595ea53631cb27011139b35d7431c
Rikelme alves <rikelmezika.12@gmail.com>
2023-06-12 22:57:02 -0300

    Criando um DashBoard

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const salesData = [
+    { id: 1, date: '2023-01-01', amount: 100 },
+    { id: 2, date: '2023-01-02', amount: 150 },
+    { id: 3, date: '2023-01-03', amount: 200 },
+  ];
+
 router.get('/', (req, res)=>{
-    res.render('home/home')
+    res.render('home/home', {sales: salesData})
 })
 
 module.exports = router
